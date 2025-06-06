cd6bf8ed9d7b7a6298fa5ef4da51dcc740cefa49
CLOUDYURI9 <161759892+CLOUDYURI9@users.noreply.github.com>
2025-06-06 20:02:07 -0300

    Add files via upload

---
+++ ConsoleAppPOO/POO/Model/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPOO.Model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public Produto() {
+        
+        }
+
+        public Produto(int id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+        }
+
+        public void Apresentar(Produto produto)
+        {
+            Console.WriteLine($"ID: {produto.Id}\n " +
+                $"Descrição: {produto.Descricao}");
+        }
+
+
+
+
+    }
+}
---
+++ ConsoleAppPOO/POO/Program.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleAppPOO.Model;
+using ConsoleAppPOO.Controller;
+using System.ComponentModel;
+/*
+
+Produto produto = new Produto();
+
+List<Produto> listaProduto = new List<Produto>();
+
+produto.Id = 1;
+produto.Descricao = "bola";
+
+listaProduto.Add(produto);
+
+
+//Apresentar apenas 1 produto
+//produto.Apresentar()
+
+
+produto = new Produto();    
+produto.Id = 2;
+produto.Descricao = "Chuteira";
+
+listaProduto.Add(produto);
+
+//Construtor com parâmetros
+Produto produto2 = new Produto(3, "Luva");
+
+
+listaProduto.Add(produto2);
+
+produto.ApresentarLista(listaProduto);
+*/
+
+BolaController controller = new BolaController();
+List<Bola> lista = new List<Bola>();
+
+
+Bola bola = new Bola(1, "Topper", 150m);
+lista = controller.Adicionar(bola);
+
+bola = new Bola(2, "Adidas", 300m);
+lista = controller.Adicionar(bola);
+
+bola = new Bola(3, "Nike", 330m);
+lista = controller.Adicionar(bola);
+
+controller.Listar(lista);
